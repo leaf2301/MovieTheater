03e7841ca38f1ed03bee754c77972431612531f9
Trần Trung Hiếu <108396149+leaf2301@users.noreply.github.com>
2023-05-15 21:26:13 +0700

    Initial Commit

---
+++ MovieTheater/MovieTheaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTheaterApp.swift
+//  MovieTheater
+//
+//  Created by Tran Hieu on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieTheaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
